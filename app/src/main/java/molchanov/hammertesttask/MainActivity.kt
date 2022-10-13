70a5d52d9033b2453a6cff180e578e683205b4fd
lbuckl <ya_mvn@mail.ru>
2022-10-13 18:00:56 +0500

    Первая рабочая версия UI с CollapsingToolbarLayout. - Верхний бар с рекламой скрывается - Планка с категориями пока не прилипает к верхнему бару - Содержимое скрола пока не отображается

--- app/src/main/java/molchanov/hammertesttask/MainActivity.kt
+++ app/src/main/java/molchanov/hammertesttask/MainActivity.kt
@@ -2,11 +2,18 @@ package molchanov.hammertesttask
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import molchanov.hammertesttask.ui.MarketMenuFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //if(savedInstanceState==null){
+            supportFragmentManager.beginTransaction()
+                .add(R.id.main_container,MarketMenuFragment.newInstance())
+                .commit()
+        //}
     }
 }
---
+++ app/src/main/java/molchanov/hammertesttask/ui/MarketMenuFragment.kt
@@ -0,0 +1,33 @@
+package molchanov.hammertesttask.ui
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import molchanov.hammertesttask.databinding.FragmentMarkertMenuBinding
+
+class MarketMenuFragment:Fragment() {
+
+    companion object {
+        //lateinit var viewModel: WeatherListViewModel
+        fun newInstance() = MarketMenuFragment()
+    }
+
+    private var _binding: FragmentMarkertMenuBinding? = null
+    private val binding: FragmentMarkertMenuBinding
+    get() {
+        return _binding!!
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?): View {
+
+        Log.v("@@@","Start")
+        _binding = FragmentMarkertMenuBinding.inflate(inflater)
+        return binding.root
+    }
+}
